f05c665620eefa01c82b3091d2da9aa2a209144f
Justin Hollis <justin.hollis@wilder.org>
2025-10-03 15:58:57 -0500

    Adjusting the trend grid aesthetics for better embedding

--- src/components/TrendGrid.js
+++ src/components/TrendGrid.js
@@ -8,7 +8,7 @@ const chartIframes = [
     src="https://datawrapper.dwcdn.net/Fg28w/1/"
     scrolling="no"
     frameBorder="0"
-    style={{ border: "none", width: "100%", height: "420px", borderRadius: "8px", background: "#fff" }}
+    style={{ border: "none", width: "100%", height: "380px", borderRadius: "8px", background: "#fff" }}
     data-external="1"
     key="Fg28w"
   />,
@@ -19,7 +19,7 @@ const chartIframes = [
     src="https://datawrapper.dwcdn.net/7AeYK/1/"
     scrolling="no"
     frameBorder="0"
-    style={{ border: "none", width: "100%", height: "420px", borderRadius: "8px", background: "#fff" }}
+    style={{ border: "none", width: "100%", height: "380px", borderRadius: "8px", background: "#fff" }}
     data-external="1"
     key="7AeYK"
   />,
@@ -30,7 +30,7 @@ const chartIframes = [
     src="https://datawrapper.dwcdn.net/XbwdO/1/"
     scrolling="no"
     frameBorder="0"
-    style={{ border: "none", width: "100%", height: "420px", borderRadius: "8px", background: "#fff" }}
+    style={{ border: "none", width: "100%", height: "380px", borderRadius: "8px", background: "#fff" }}
     data-external="1"
     key="XbwdO"
   />,
@@ -41,7 +41,7 @@ const chartIframes = [
     src="https://datawrapper.dwcdn.net/vZwDX/1/"
     scrolling="no"
     frameBorder="0"
-    style={{ border: "none", width: "100%", height: "420px", borderRadius: "8px", background: "#fff" }}
+    style={{ border: "none", width: "100%", height: "380px", borderRadius: "8px", background: "#fff" }}
     data-external="1"
     key="vZwDX"
   />,
@@ -52,7 +52,7 @@ const chartIframes = [
     src="https://datawrapper.dwcdn.net/vPzR3/1/"
     scrolling="no"
     frameBorder="0"
-    style={{ border: "none", width: "100%", height: "420px", borderRadius: "8px", background: "#fff" }}
+    style={{ border: "none", width: "100%", height: "380px", borderRadius: "8px", background: "#fff" }}
     data-external="1"
     key="vPzR3"
   />,
@@ -61,19 +61,41 @@ const chartIframes = [
 
 const TrendGrid = () => (
   <div style={{
-    display: "flex",
-    flexDirection: "row",
-    gap: "24px",
-    justifyContent: "flex-start",
-    alignItems: "flex-end",
     width: "100%",
-    margin: "2px 0"
+    margin: "32px 0"
   }}>
-    {chartIframes.map((iframe, idx) => (
-      <div key={idx} style={{ flex: 1, minWidth: 0 }}>
-        {iframe}
-      </div>
-    ))}
+    <div
+      style={{
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: "505px",           // y position
+    zIndex: 10,             // in front of charts
+    fontSize: "8pt",
+    color: "#666",
+    textAlign: "left",
+    fontStyle: "italic",
+    pointerEvents: "none",
+    background: "white",
+    padding: "4px 16px", 
+      }}
+    >
+      Source: Minnesota Department of Employment and Economic Development (DEED)
+    </div>
+    <div style={{
+      display: "flex",
+      flexDirection: "row",
+      gap: "24px",
+      justifyContent: "center",
+      alignItems: "flex-start",
+      width: "100%"
+    }}>
+      {chartIframes.map((iframe, idx) => (
+        <div key={idx} style={{ flex: 1, minWidth: 0 }}>
+          {iframe}
+        </div>
+      ))}
+    </div>
   </div>
 );
 
